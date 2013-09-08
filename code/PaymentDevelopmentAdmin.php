dd5f2eabab61bff832597ff9872a2176e1fbedc7
Jeremy Shipman <jeremy@burnbright.net>
2013-09-09 09:01:19 +1200

    Introduced dev ui for viewing payment option capabilities

---
+++ code/PaymentDevelopmentAdmin.php
@@ -0,0 +1,72 @@
+<?php
+
+class PaymentDevelopmentAdmin extends Controller{
+	
+
+	function index(){
+
+		$renderer = DebugView::create();
+
+		$renderer->writeHeader();
+		$renderer->writeInfo("Payment Gateways", Director::absoluteBaseURL());
+		$base = Director::baseURL();
+
+		$types = $this->PaymentTypes();
+
+		echo "<table style=\"font-size:12px;\" border=1 cellspacing=0>
+				<thead>
+					<tr>
+						<td>Short Name</td>
+						<td>Full name</td>
+						<td>Authorize</td>
+						<td>CompleteAuthorize</td>
+						<td>Capture</td>
+						<td>Complete Purchase</td>
+						<td>Refund</td>
+						<td>Void</td>
+						<td>Create Card</td>
+						<td>Delete Card</td>
+						<td>Update Card</td>
+					</tr>
+				</thead>
+			<tbody>";
+
+		foreach($types as $gateway){
+
+			echo "<tr>".
+				"<td>".$gateway->getShortName()."</td>".
+				"<td>".$gateway->getName()."</td>".
+				"<td>".($gateway->supportsAuthorize() ? "yes" : "")."</td>".
+				"<td>".($gateway->supportsCompleteAuthorize() ? "yes" : "")."</td>".
+				"<td>".($gateway->supportsCapture() ? "yes" : "")."</td>".
+				"<td>".($gateway->supportsCompletePurchase() ? "yes" : "")."</td>".
+				"<td>".($gateway->supportsRefund() ? "yes" : "")."</td>".
+				"<td>".($gateway->supportsVoid() ? "yes" : "")."</td>".
+				"<td>".($gateway->supportsCreateCard() ? "yes" : "")."</td>".
+				"<td>".($gateway->supportsDeleteCard() ? "yes" : "")."</td>".
+				"<td>".($gateway->supportsUpdateCard() ? "yes" : "")."</td>".
+			"</tr>";
+
+			if($this->request->getVar('defaults')){
+				echo "<tr><td colspan=\"11\">";
+					var_dump($gateway->getDefaultParameters());
+				echo "</td></tr>";
+			}
+
+		}
+		echo "</tbody></table>";
+		$renderer->writeFooter();
+	}
+
+	/**
+	 * Get all available payment types
+	 */
+	private function PaymentTypes(){
+		$gateways =  Omnipay\Common\GatewayFactory::find();
+		$gateways = array_map(function($name) {
+	        return Omnipay\Common\GatewayFactory::create($name);
+	    }, $gateways);
+	    return $gateways;
+	}
+
+}

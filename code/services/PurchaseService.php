647dfb35efe5a3fa301dcc47076de79740c96e12
Ingo Schommer <ingo@silverstripe.com>
2014-06-04 14:31:07 +1200

    Critical regression from 964fb14
    This breaks onCaptured, since it checks for `TotalOutstanding` via `Payable->TotalPaid()`, which in turn uses `the Order->Payments()` relationship. Since `Status=Captured` isn't written at this point, the result is `>0`, failing the order.

--- code/services/PurchaseService.php
+++ code/services/PurchaseService.php
@@ -103,12 +103,13 @@ class PurchaseService extends PaymentService{
 			if ($response->isSuccessful()) {
 				$this->createMessage('PurchasedResponse', $response);
 				$this->payment->Status = 'Captured';
+				$this->payment->write();
 				$this->payment->extend('onCaptured', $gatewayresponse);
 			} else {
 				$this->createMessage('CompletePurchaseError', $response);
 				$this->payment->Status = 'Void';
+				$this->payment->write();
 			}
-			$this->payment->write();
 		} catch (Omnipay\Common\Exception\OmnipayException $e) {
 			$this->createMessage("CompletePurchaseError", $e);
 		}

3283852eda99068258b1bcb7013cbbba687b8284
Mark Guinn <mark@adaircreative.com>
2014-07-22 15:10:01 +0100

    Fixed a few more bugs

--- code/services/SavedCardService.php
+++ code/services/SavedCardService.php
@@ -8,95 +8,95 @@ use Omnipay\Common\CreditCard;
  */
 class SavedCardService extends PaymentService {
 
-    /**
-     * Attempt to save a new credit card.
-     *
-     * @param  array $data
-     * @return \Omnipay\Common\Message\ResponseInterface omnipay's response class, specific to the chosen gateway.
-     */
-    public function createCard($data = array()) {
-        if ($this->payment->Status !== "Created") {
-            return null; //could be handled better? send payment response?
-        }
-        if (!$this->payment->isInDB()) {
-            $this->payment->write();
-        }
+	/**
+	 * Attempt to save a new credit card.
+	 *
+	 * @param  array $data
+	 * @return \Omnipay\Common\Message\ResponseInterface omnipay's response class, specific to the chosen gateway.
+	 */
+	public function createCard($data = array()) {
+		if ($this->payment->Status !== "Created") {
+			return null; //could be handled better? send payment response?
+		}
+		if (!$this->payment->isInDB()) {
+			$this->payment->write();
+		}
 
-        // if they didn't give us a name, create one from the masked number
-        if (empty($data['name'])) {
-            $data['name'] = preg_replace('/[^0-9]/', '', $data['number']);               // normalize out dashes and spaced
-            $data['name'] = preg_replace('/[0-9]/', '*', $data['name']);                 // replace numbers
-            $data['name'] = substr($data['name'], 0, -4) . substr($data['number'], -4);  // swap in the last 4 digits
-        }
+		// if they didn't give us a name, create one from the masked number
+		if (empty($data['cardName'])) {
+			$data['cardName'] = preg_replace('/[^0-9]/', '', $data['number']);               // normalize out dashes and spaced
+			$data['cardName'] = preg_replace('/[0-9]/', '*', $data['cardName']);                 // replace numbers
+			$data['cardName'] = substr($data['cardName'], 0, -4) . substr($data['number'], -4);  // swap in the last 4 digits
+		}
 
-        $message = $this->createMessage('CreateCardRequest');
-        $message->write();
+		$message = $this->createMessage('CreateCardRequest');
+		$message->write();
 
-        $request = $this->oGateway()->createCard(array_merge(
-            $data,
-            array(
-                'card' => $this->getCreditCard($data),
-                'clientIp' => isset($data['clientIp']) ? $data['clientIp'] : null,
-            )
-        ));
-        $this->logToFile($request->getParameters(), "CreateCardRequest_post");
+		$request = $this->oGateway()->createCard(array_merge(
+			$data,
+			array(
+				'card' => $this->getCreditCard($data),
+				'clientIp' => isset($data['clientIp']) ? $data['clientIp'] : null,
+			)
+		));
+		$this->logToFile($request->getParameters(), "CreateCardRequest_post");
 
-        $gatewayresponse = $this->createGatewayResponse();
-        try {
-            $response = $this->response = $request->send();
-            $gatewayresponse->setOmnipayResponse($response);
+		$gatewayresponse = $this->createGatewayResponse();
+		try {
+			$response = $this->response = $request->send();
+			$gatewayresponse->setOmnipayResponse($response);
 
-            //update payment model
-            if ($response->isSuccessful()) {
-                //successful payment
-                $this->createMessage('CreateCardResponse', $response);
-                $gatewayresponse->setMessage("Card created successfully");
+			//update payment model
+			if ($response->isSuccessful()) {
+				//successful payment
+				$this->createMessage('CreateCardResponse', $response);
+				$gatewayresponse->setMessage("Card created successfully");
 
-                // create the saved card
-                $card = new SavedCreditCard(array(
-                    'CardReference' => $response->getCardReference(),
-                    'LastFourDigits' => substr($data['number'], -4),
-                    'Name' => $data['name'],
-                    'UserID' => Member::currentUserID(),
-                ));
+				// create the saved card
+				$card = new SavedCreditCard(array(
+					'CardReference'  => $response->getCardReference(),
+					'LastFourDigits' => substr($data['number'], -4),
+					'Name'           => $data['cardName'],
+					'UserID'         => Member::currentUserID(),
+				));
 
-                $card->write();
+				$card->write();
 
-                $this->payment->SavedCardID = $card->ID;
-                $this->payment->write();
-            } else {
-                //handle error
-                $this->createMessage('CreateCardError', $response);
-                $gatewayresponse->setMessage(
-                    "Error (".$response->getCode()."): ".$response->getMessage()
-                );
-            }
-        } catch (Omnipay\Common\Exception\OmnipayException $e) {
-            $this->createMessage('CreateCardError', $e);
-            $gatewayresponse->setMessage($e->getMessage());
-        }
+				$this->payment->SavedCreditCardID = $card->ID;
+				$this->payment->write();
+			} else {
+				//handle error
+				$this->createMessage('CreateCardError', $response);
+				$gatewayresponse->setMessage(
+					"Error (".$response->getCode()."): ".$response->getMessage()
+				);
+			}
+		} catch (Omnipay\Common\Exception\OmnipayException $e) {
+			$this->createMessage('CreateCardError', $e);
+			$gatewayresponse->setMessage($e->getMessage());
+		}
 
-        // not sure if this is needed
-        $gatewayresponse->setRedirectURL($this->getRedirectURL());
+		// not sure if this is needed
+		$gatewayresponse->setRedirectURL($this->getRedirectURL());
 
-        return $gatewayresponse;
-    }
+		return $gatewayresponse;
+	}
 
-    public function updateCard(SavedCreditCard $card, $data = array()) {
-        // TODO
-    }
+	public function updateCard(SavedCreditCard $card, $data = array()) {
+		// TODO
+	}
 
-    public function deleteCard(SavedCreditCard $card) {
-        // TODO
-    }
+	public function deleteCard(SavedCreditCard $card) {
+		// TODO
+	}
 
-    /**
-     * @param array $data
-     * @return \Omnipay\Common\CreditCard
-     */
-    protected function getCreditCard($data) {
-        return new CreditCard($data);
-    }
+	/**
+	 * @param array $data
+	 * @return \Omnipay\Common\CreditCard
+	 */
+	protected function getCreditCard($data) {
+		return new CreditCard($data);
+	}
 
 
 }
--- tests/SavedCardServiceTest.php
+++ tests/SavedCardServiceTest.php
@@ -2,97 +2,97 @@
 
 class SavedCardServiceTest extends PaymentTest {
 
-    public function testCreateCard() {
-        $payment = $this->payment->setGateway('Stripe');
-        $service = new SavedCardService($payment);
-        $this->setMockHttpResponse('Stripe/Mock/CreateCardSuccess.txt');
-        $response = $service->createCard(array(
-            'name' => 'My Test Card 1',
-            'number' => '4111111111111111',
-            'expiryMonth' => '5',
-            'expiryYear' => date("Y", strtotime("+1 year"))
-        ));
-        $this->assertTrue($response->isSuccessful());
-        $this->assertFalse($response->isRedirect());
-        $this->assertSame("Created", $payment->Status, 'does not process payment');
+	public function testCreateCard() {
+		$payment = $this->payment->setGateway('Stripe');
+		$service = new SavedCardService($payment);
+		$this->setMockHttpResponse('Stripe/Mock/CreateCardSuccess.txt');
+		$response = $service->createCard(array(
+			'cardName' => 'My Test Card 1',
+			'number' => '4111111111111111',
+			'expiryMonth' => '5',
+			'expiryYear' => date("Y", strtotime("+1 year"))
+		));
+		$this->assertTrue($response->isSuccessful());
+		$this->assertFalse($response->isRedirect());
+		$this->assertSame("Created", $payment->Status, 'does not process payment');
 
-        /** @var SavedCreditCard $card */
-        $card = $payment->SavedCreditCard();
-	    Debug::dump($card);
-        $this->assertNotNull($card);
-        $this->assertInstanceOf('SavedCreditCard', $card);
-        $this->assertEquals('My Test Card 1', $card->Name);
-        $this->assertEquals('1111', $card->LastFourDigits);
-        $this->assertEquals('cus_1MZSEtqSghKx99', $card->CardReference);
+		/** @var SavedCreditCard $card */
+		$card = $payment->SavedCreditCard();
+		Debug::dump($card);
+		$this->assertNotNull($card);
+		$this->assertInstanceOf('SavedCreditCard', $card);
+		$this->assertEquals('My Test Card 1', $card->Name);
+		$this->assertEquals('1111', $card->LastFourDigits);
+		$this->assertEquals('cus_1MZSEtqSghKx99', $card->CardReference);
 
-        //check messaging
-        $this->assertDOSContains(array(
-            array('ClassName' => 'CreateCardRequest'),
-            array('ClassName' => 'CreateCardResponse'),
-        ), $payment->Messages());
-    }
+		//check messaging
+		$this->assertDOSContains(array(
+			array('ClassName' => 'CreateCardRequest'),
+			array('ClassName' => 'CreateCardResponse'),
+		), $payment->Messages());
+	}
 
-    public function testCreateCardWithNoName() {
-        $payment = $this->payment->setGateway('Stripe');
-        $service = new SavedCardService($payment);
-        $this->setMockHttpResponse('Stripe/Mock/CreateCardSuccess.txt');
-        $response = $service->createCard(array(
-            'number' => '4111-1111-1111-1111',
-            'expiryMonth' => '5',
-            'expiryYear' => date("Y", strtotime("+1 year"))
-        ));
-        $this->assertTrue($response->isSuccessful());
+	public function testCreateCardWithNoName() {
+		$payment = $this->payment->setGateway('Stripe');
+		$service = new SavedCardService($payment);
+		$this->setMockHttpResponse('Stripe/Mock/CreateCardSuccess.txt');
+		$response = $service->createCard(array(
+			'number' => '4111-1111-1111-1111',
+			'expiryMonth' => '5',
+			'expiryYear' => date("Y", strtotime("+1 year"))
+		));
+		$this->assertTrue($response->isSuccessful());
 
-        /** @var SavedCreditCard $card */
-        $card = $payment->SavedCreditCard();
-        $this->assertEquals('************1111', $card->Name);
-        $this->assertEquals('1111', $card->LastFourDigits);
-    }
+		/** @var SavedCreditCard $card */
+		$card = $payment->SavedCreditCard();
+		$this->assertEquals('************1111', $card->Name);
+		$this->assertEquals('1111', $card->LastFourDigits);
+	}
 
-    public function testUpdateCard() {
-        $this->markTestIncomplete('Need to test/implement updateCard');
-    }
+	public function testUpdateCard() {
+		$this->markTestIncomplete('Need to test/implement updateCard');
+	}
 
-    public function testDeleteCard() {
-        $this->markTestIncomplete('Need to test/implement deleteCard');
-    }
+	public function testDeleteCard() {
+		$this->markTestIncomplete('Need to test/implement deleteCard');
+	}
 
-    public function testPayWithCard() {
-        $card = $this->getTestPaymentWithSavedCard()->SavedCreditCard();
-        $payment = $this->payment->setGateway('Stripe');
-        $payment->setAmount(10.00);
-        $payment->setCurrency('NZD');
-        $payment->SavedCardID = $card->ID;
-        $payment->write();
-        $service = new PurchaseService($payment);
-        $this->setMockHttpResponse('Stripe/Mock/PurchaseSuccess.txt');//add success mock response from file
-        $response = $service->purchase();
-        $this->assertTrue($response->isSuccessful());
-        $this->assertFalse($response->isRedirect());
-        $this->assertSame("Captured", $payment->Status, "has the payment been captured");
+	public function testPayWithCard() {
+		$card = $this->getTestPaymentWithSavedCard()->SavedCreditCard();
+		$payment = $this->payment->setGateway('Stripe');
+		$payment->setAmount(10.00);
+		$payment->setCurrency('NZD');
+		$payment->SavedCardID = $card->ID;
+		$payment->write();
+		$service = new PurchaseService($payment);
+		$this->setMockHttpResponse('Stripe/Mock/PurchaseSuccess.txt');//add success mock response from file
+		$response = $service->purchase();
+		$this->assertTrue($response->isSuccessful());
+		$this->assertFalse($response->isRedirect());
+		$this->assertSame("Captured", $payment->Status, "has the payment been captured");
 
-        //check messaging
-        $this->assertDOSContains(array(
-            array('ClassName' => 'PurchaseRequest'),
-            array('ClassName' => 'PurchasedResponse')
-        ), $payment->Messages());
-    }
+		//check messaging
+		$this->assertDOSContains(array(
+			array('ClassName' => 'PurchaseRequest'),
+			array('ClassName' => 'PurchasedResponse')
+		), $payment->Messages());
+	}
 
 
-    /**
-     * @return Payment
-     */
-    protected function getTestPaymentWithSavedCard() {
-        $payment = $this->payment->setGateway('Stripe');
-        $payment->setAmount(0);
-        $service = new SavedCardService($payment);
-        $this->setMockHttpResponse('Stripe/Mock/CreateCardSuccess.txt');
-        $response = $service->createCard(array(
-            'name' => 'My Test Card 1',
-            'number' => '4111111111111111',
-            'expiryMonth' => '5',
-            'expiryYear' => date("Y", strtotime("+1 year"))
-        ));
-        return $payment;
-    }
+	/**
+	 * @return Payment
+	 */
+	protected function getTestPaymentWithSavedCard() {
+		$payment = $this->payment->setGateway('Stripe');
+		$payment->setAmount(0);
+		$service = new SavedCardService($payment);
+		$this->setMockHttpResponse('Stripe/Mock/CreateCardSuccess.txt');
+		$response = $service->createCard(array(
+			'cardName' => 'My Test Card 1',
+			'number' => '4111111111111111',
+			'expiryMonth' => '5',
+			'expiryYear' => date("Y", strtotime("+1 year"))
+		));
+		return $payment;
+	}
 }

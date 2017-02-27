2d1ee4af50a5e054d3b5a08919b88d76102c2788
Chris Lock <code@catharsis.co.uk>
2017-02-27 19:15:46 +0000

    Changing to be a bigger field for Sagepay response (#162)
    Changing `Reference` field to Varchar(255), as some gateways (sagepay) create lengthy transaction references.

--- code/model/messaging/GatewayMessage.php
+++ code/model/messaging/GatewayMessage.php
@@ -5,7 +5,7 @@ class GatewayMessage extends PaymentMessage
 
     private static $db = array(
         "Gateway" => "Varchar",
-        "Reference" => "Varchar", //remote id
+        "Reference" => "Varchar(255)", //remote id
         "Code" => "Varchar"
     );
 

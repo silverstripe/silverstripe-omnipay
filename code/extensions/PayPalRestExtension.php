66a10cbafefce963a43a9ad576edfb1797570d94
Chris Lock <chris.l@biffbangpow.com>
2017-07-26 10:57:22 +0100

    Added check for Rest Gateway

--- code/extensions/PayPalRestExtension.php
+++ code/extensions/PayPalRestExtension.php
@@ -20,7 +20,9 @@ class PayPalRestExtension extends Extension
          * If you don't want to supply a Credit card then unsetting this will
          * take you through to PayPal without one
          */
-        unset($gatewayData['card']);
+        if ($this->isRestGateway()) {
+            unset($gatewayData['card']);
+        }
     }
 
     /**
@@ -31,10 +33,20 @@ class PayPalRestExtension extends Extension
         /**
          * As described in Omnipay\PayPal\Message\RestPurchaseRequest PayPal
          * responds from the payment with the transaction reference and a
-         * PayerID as GET vars. We gather tham and throw them back to PayPal
+         * PayerID as GET vars. We gather them and throw them back to PayPal
          * for confirmation
          */
-        $gatewayData['transactionReference'] = $_GET['paymentId'];
-        $gatewayData['payerId'] = $_GET['PayerID'];
+        if ($this->isRestGateway()) {
+            $gatewayData['transactionReference'] = $_GET['paymentId'];
+            $gatewayData['payerId'] = $_GET['PayerID'];
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function isRestGateway()
+    {
+        return ($this->owner->getPayment()->Gateway === 'PayPal_Rest');
     }
 }
